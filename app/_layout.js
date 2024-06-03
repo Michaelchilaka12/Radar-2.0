a8a63e1be58adb097a2963e0acfd008f5aa57723
OATheSWE <osborneosas12@gmail.com>
2024-06-03 23:56:14 +0100

    feat: Optimized prod branch for production

--- app/_layout.js
+++ app/_layout.js
@@ -1,15 +1,12 @@
-import { MantineProvider } from "@mantine/core";
-import "@mantine/core/styles.css";
-import "../global.css";
-import { Slot} from 'expo-router'
+
 
 
 
 const App = () => {
   return (
-    <MantineProvider>
-       <Slot />
-    </MantineProvider>
+    <div>
+      
+    </div>
   );
 };
 
